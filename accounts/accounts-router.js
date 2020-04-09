0f1d4b4a112908e4e3aa39b768f1dde4f1061d1c
Daniel Morales <daniel.a.moralesalt@gmail.com>
2020-04-08 22:52:41 -0400

    Wrote first endpoint, GET request

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -6,4 +6,12 @@ const router = express.Router();
 
 // endpoints
 
+router.get("/", (req, res) => {
+	db("accounts")
+		.then((accounts) => res.status(200).json(accounts))
+		.catch((error) =>
+			res.status(500).json({ message: "Error retrieving accounts", error })
+		);
+});
+
 module.exports = router;
